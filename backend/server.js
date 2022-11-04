618bf4410455b079575cbe8828c7fc8ff2df2c45
beDry2000 <106262487+beDry2000@users.noreply.github.com>
2022-11-04 14:47:18 +0700

    cors origin

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 const io = new Server(server, {
+    // cors: {
+    //     origin: 'http://localhost:3000'
+    // }
+
     cors: {
-        origin: 'http://localhost:3000'
+        origin: process.env.ORIGIN
     }
 });
 
